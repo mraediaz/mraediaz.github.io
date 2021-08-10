28f36d63933a039a77eeb69c230f5e4f9ade39b5
unknown <mraediaz@gmail.com>
2021-08-10 18:52:02 +0200

    houseplant growth

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var images = ["images/flower/2.png", "images/flower/3.png", "images/flower/4.png", "images/flower/5.png", "images/flower/6.png", "images/flower/7.png", "images/flower/8.png", "images/flower/9.png", "images/flower/10.png", "images/flower/11.png", "images/flower/12.png", "images/flower/13.png", "images/flower/14.png", "images/flower/15.png", "images/flower/16.png"]
+var images = ["images/plants/1.png", "images/plants/2.png", "images/plants/3.png", "images/plants/4.png", "images/plants/5.png", "images/plants/6.png", "images/plants/7.png", "images/plants/8.png", "images/plants/9.png", "images/plants/10.png", "images/plants/11.png", "images/plants/12.png", "images/plants/13.png", "images/plants/14.png", "images/plants/15.png", "images/plants/16.png", "images/plants/17.png", "images/plants/18.png", "images/plants/19.png", "images/plants/20.png"]
 
 var imgState = 0;
 
 var imgTag = document.getElementById("imgClickAndChange");
 
 imgTag.addEventListener("click", function (event) {
-  imgState = (++imgState % 15);
+  imgState = (++imgState % 20);
     event.target.src = images[imgState];
 });
 
