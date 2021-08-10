4ad100bcc9d1ce995a88ce711aa898b76aa729b0
unknown <mraediaz@gmail.com>
2021-08-10 14:40:50 +0200

    FLOWER ADDITION

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var images = ["https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=350", "https://i2.wp.com/beebom.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg?resize=640%2C426", "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350"]
+var images = ["images/flower/2.PNG", "images/flower/3.PNG", "images/flower/4.PNG", "images/flower/5.PNG", "images/flower/6.PNG", "images/flower/7.PNG", "images/flower/8.PNG", "images/flower/9.PNG", "images/flower/10.PNG", "images/flower/11.PNG", "images/flower/12.PNG", "images/flower/13.PNG", "images/flower/14.PNG", "images/flower/15.PNG", "images/flower/16.PNG"]
 
 var imgState = 0;
 
 var imgTag = document.getElementById("imgClickAndChange");
 
 imgTag.addEventListener("click", function (event) {
-  imgState = (++imgState % 3);
+  imgState = (++imgState % 15);
     event.target.src = images[imgState];
 });
+
